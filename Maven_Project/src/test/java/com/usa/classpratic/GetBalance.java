b2a04ccb487364ccc9966f2b1667b8f6cb2fe4b9
tasfiqkalam <tasfiqkalam@192.168.0.3>
2018-01-28 17:47:01 -0500

    versionfeb3

---
+++ Maven_Project/src/test/java/com/usa/classpratic/GetBalance.java
@@ -0,0 +1,15 @@
+package com.usa.classpratic;
+
+public class GetBalance {
+	
+
+ public void getbalance(){
+	 System.out.println("I have only $ 1000 balance");
+ }
+ public static void main(String[] args) {
+	
+
+ GetBalance obj = new GetBalance ();
+ obj.getbalance();
+ 
+}}
